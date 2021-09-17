af0420d3b3844127b0e46db56475aeb5e11f0cce
SkyAo <csvwolf@qq.com>
2021-09-18 00:33:23 +0800

    fix: reset unlimit pool close failed

--- unlimited_pool.js
+++ unlimited_pool.js
@@ -21,16 +21,14 @@ var UnlimitedBtn = readImage('./assets/unlimited/10.jpg')
 var Zero = readImage('./assets/unlimited/zero.jpg')
 // 重置无限池
 var Reset = readImage('./assets/unlimited/reset.jpg')
-var Execute = readImage('./assets/unlimited/execute.jpg')
-var Close = readImage('./assets/unlimited/close.jpg')
+var Execute = [1582, 861]
+var Close = [1268, 840]
 var i = 0
 
 events.on('exit', function() {
   UnlimitedBtn.recycle()
   Zero.recycle()
   Reset.recycle()
-  Execute.recycle()
-  Close.recycle()
   toast('共抽 ' + i + ' 次')
 })
 
@@ -44,19 +42,16 @@ while (true) {
       continue
     }
     click1(resetPosition[0], resetPosition[1], false)
-    sleep1(100)
-    const executePosition = findButton(Execute, { maxTimes: 1 })
-    if (!executePosition) {
-      continue
-    }
-    click1(executePosition[0], executePosition[1], false)
-    sleep(200)
-    const closePosition = findButton(Close, { maxTimes: 1})
-    if (!closePosition) {
-      continue
-    }
-    click1(closePosition[0], closePosition[1], false)
-    sleep(300)
+    sleep1(500)
+    // const executePosition = findButton(Execute, { maxTimes: 1 })
+    // if (!executePosition) {
+    //   console.log('找不到 execute')
+    //   continue
+    // }
+    click1(Execute[0], Execute[1], true)
+    sleep(4000)
+    click1(Close[0], Close[1], true)
+    sleep(1000)
   }
 
   const position = findButton(UnlimitedBtn, { maxTimes: 5 })
@@ -64,7 +59,7 @@ while (true) {
     click1(position[0], position[1], false)
     toast('抽完了一次！')
     // 在这里才开始
-    click1(Blank[0], Blank[1], true)
+    // click1(Blank[0], Blank[1], true)
     i++
   }
 
